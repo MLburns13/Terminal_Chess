f8f9f77069e8ea0ed78da11178270696732c533d
Michael <ercf77@gmail.com>
2024-10-18 16:13:24 -0400

    initial commit

---
+++ board.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Board {
+    private:
+        int arr[64];
+        /*
+        pawn = 1
+        bishop = 2
+        knight = 3
+        rook = 4
+        queen = 5
+        king = 6
+        */
+
+    public:
+        Board() {
+            
+        }
+};
